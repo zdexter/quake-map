39df5c22551617bc16d0892d93a3e9421482026b
Zach Dexter <zach.dexter@gmail.com>
2013-08-24 13:34:03 -0400

    Screenshots and JSON view

--- fe/views/dash.py
+++ fe/views/dash.py
@@ -1,6 +1,8 @@
 from flask import render_template
+from flask.json import jsonify
 from fe import app
 from fe.models.dal.event import EventDAL
+from collections import defaultdict
 
 @app.route('/')
 def dashView(methods=['GET']):
@@ -19,4 +21,15 @@ def quakeListView(methods=['GET']):
         display_quakes.append(((quake.event.lng, quake.event.lat),
                 [(city.name, distance) for city, distance in nearby_cities]))
     return render_template('quake_list.html', quakes=display_quakes)
+
+@app.route('/quakes/list_json')
+def quakeListJsonView(methods=['GET']):
+    quakes = EventDAL.getAllQuakes()
+    display_quakes = defaultdict(str)
+    # Same caveat as the above view
+    for quake in quakes:
+        nearby_cities = EventDAL.getNearbyCities(quake.event)
+        quake_name = '{},{}'.format(quake.event.lng, quake.event.lat)
+        display_quakes[quake_name] = [(city.name, distance) for city, distance in nearby_cities]
+    return jsonify(quakes=display_quakes)
 
